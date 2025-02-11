63641f9c4df4cb2a4dca93fd9468e9ecf88b2e64
vinoothnaveedhi <51501839+vinoothnaveedhi@users.noreply.github.com>
2025-02-11 22:04:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const messages = [
+  "Buba you sure? 🥺",
+  "Baby kyuu?? 😢",
+  "Jaanu I love youuuu, Yes bolo na 😭",
+  "Don't do this to me... 😞",
+  "Itna rude ho aap 💀",
+  "Fine, I give up... Just kidding! 😏",
+  "Babu Pyar karti hun aapse, choose yes 👉👈",
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+  const noButton = document.querySelector('.no-button');
+  const yesButton = document.querySelector('.yes-button');
+
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.2}px`;  // Increases size with each "No"
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
